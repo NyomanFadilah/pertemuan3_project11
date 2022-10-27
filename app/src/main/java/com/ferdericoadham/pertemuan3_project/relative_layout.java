89443cfe96bd101f212fea02bac5c062a3c9d739
Mobile 1 <2010031805007@sar.ac.id>
2022-10-27 12:54:41 +0700

    Tugas

--- app/src/main/java/com/ferdericoadham/pertemuan3_project/relative_layout.java
+++ app/src/main/java/com/ferdericoadham/pertemuan3_project/relative_layout.java
@@ -1,8 +1,12 @@
 package com.ferdericoadham.pertemuan3_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class relative_layout extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class relative_layout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+        Button buttonexplicit = findViewById(R.id.button2);
+        Button buttonimplicit = findViewById(R.id.button3);
+
+
+        buttonexplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+            }
+        });
+
+
+        buttonimplicit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.iweblogsite.com"));
+
+            }
+        });
+
+
     }
 }
